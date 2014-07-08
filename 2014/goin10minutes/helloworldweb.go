12dd86feb81a7190181ed2b610df28c6b27453b4
Alexander Fahlke <alexander.fahlke@gfk.com>
2014-07-08 13:39:14 +0200

    added Go In 10 Minutes talk

---
+++ 2014/goin10minutes/helloworldweb.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8080", Log(http.DefaultServeMux))
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path[1:] != "" {
+		fmt.Fprintf(w, "Hello %s!", r.URL.Path[1:])
+	} else {
+		fmt.Fprint(w, "Hello World!")
+	}
+
+}
+
+func Log(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+		handler.ServeHTTP(w, r)
+	})
+}
